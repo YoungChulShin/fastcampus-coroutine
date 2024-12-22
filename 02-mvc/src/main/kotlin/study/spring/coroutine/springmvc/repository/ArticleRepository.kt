52ae77268b9c6938a3abfcc1fcb7bd97e8094e0a
YoungChulShin <go1323@gmail.com>
2024-12-22 19:17:03 +0900

    사용하지 않는 프로젝트 삭제

--- 02-mvc/src/main/kotlin/study/spring/coroutine/springmvc/repository/ArticleRepository.kt
+++
@@ -1,9 +0,0 @@
-package study.spring.coroutine.springmvc.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import study.spring.coroutine.springmvc.model.Article
-
-interface ArticleRepository: JpaRepository<Article, Long> {
-
-    fun findAllByTitleContains(title: String): List<Article>
-}
