a34f160d68d06962448a72126fc1f9f7a48d913b
YoungChulShin <go1323@gmail.com>
2024-12-22 19:32:47 +0900

    mvc - hello world

---
+++ spring-mvc/src/main/kotlin/studt/coroutine/springmvc/controller/HelloController.kt
@@ -0,0 +1,15 @@
+package studt.coroutine.springmvc.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello/{name}")
+    fun hello(@PathVariable name: String): String {
+        return "Hello $name ~"
+    }
+}
