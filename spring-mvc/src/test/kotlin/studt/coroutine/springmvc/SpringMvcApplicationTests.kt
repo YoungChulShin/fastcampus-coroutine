7c99065311f704f0c318987d28b023c0beb5151d
YoungChulShin <go1323@gmail.com>
2024-12-22 19:25:39 +0900

    mvc - 프로젝트 추가

---
+++ spring-mvc/src/test/kotlin/studt/coroutine/springmvc/SpringMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package studt.coroutine.springmvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
