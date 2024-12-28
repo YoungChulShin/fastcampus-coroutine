1eed891e94d4f9a07fd0161e07091b2c775552e7
YoungChulShin <go1323@gmail.com>
2024-12-28 22:57:23 +0900

    coroutine - 프로젝트 초안

---
+++ webflux-coroutine/src/main/kotlin/study/coroutine/webfluxcoroutine/WebfluxCoroutineApplication.kt
@@ -0,0 +1,11 @@
+package study.coroutine.webfluxcoroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxCoroutineApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxCoroutineApplication>(*args)
+}
